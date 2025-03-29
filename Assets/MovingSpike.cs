df838a1a77ebbba867f338dcb32ab34dc1c9ec4d
Heng2007 <zhuheng2007@gmail.com>
2025-03-28 21:00:08 -0400

    spike

---
+++ Assets/MovingSpike.cs
@@ -0,0 +1,52 @@
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class MovingSpike : MonoBehaviour
+{
+
+    static float i = -23f;
+ 
+
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        i += 0.2f;
+        if (i >= 23f)  // Reset i to -23f after reaching 23f
+        {
+            i = -23f;
+        } 
+
+        MovingHorizontally Spike = new MovingHorizontally();
+        Spike.GetT();
+
+        transform.position = Vector3.Lerp(new Vector3(i,-3.5f,0.2145f) ,new Vector3 (-i, -3.5f, 0.2145f),
+            Mathf.PingPong(Spike.GetT(), 1));
+    }
+}
+
+    class MovingHorizontally
+    {
+
+        private float _speed = 0.05f;
+        private float t;
+
+        public float GetSpeed()
+        {
+            return _speed;
+        }
+        public float GetT()
+        {
+            t += Time.deltaTime * _speed;
+            return t;
+        }
+    }  
+
+
+
